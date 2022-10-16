dd1f5ac54d4628d1918cdadfc3d1f0e62a305aa3
MenderMan <randomguys632@gmail.com>
2022-10-16 14:15:37 -0400

    stuff

---
+++ clients/2008E/content/scripts/CSMPBoot.lua
@@ -0,0 +1,3 @@
+--Load Script
+dofile('rbxasset://scripts/CSMPFunctions.lua')
+_G.CSSolo(3695,'MenderMan','PoliceCap.rbxm','NoHat.rbxm','NoHat.rbxm',24,104,26,26,26,26,'SuitWithPurpleTie.rbxm','Ninja-Suit-Of-Darkness.rbxm','NoPants.rbxm','DefaultFace.rbxm','DefaultHead.rbxm','NBC','NoExtra.rbxm',false);
